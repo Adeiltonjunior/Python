e2112b82eff2c1ee03bf227e871372998aa99f96
aajunioor <adeilton_junior1108@hotmail.com>
2025-04-02 10:16:04 -0300

    Iniciando os estudos em Python

---
+++ ex003.py
@@ -0,0 +1,5 @@
+n1=int(input('digite um número: '))
+n2=int(input('digite um número: '))
+s=n1+n2
+# print('A soma dos números equivale a: {}'.format(s))
+print('A soma dos números {} e {} equivale a: {}'.format(n1,n2,s))
